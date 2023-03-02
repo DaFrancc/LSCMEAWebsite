87d160103abd2e9bf83b87ccde8ebb71c685dd24
DaFrancc <xenonaught@gmail.com>
2023-03-01 19:16:28 -0600

    Initial commit

---
+++ membersLogicM.js
@@ -0,0 +1,28 @@
+function setDeviceType() {
+    if(window.screen.availWidth > 1000) {
+        window.location ="membersD.html";
+    }
+}
+
+function toggleTab() {
+    let nav = document.getElementById("nav-tab");
+    let tb = document.getElementById("top-bar");
+    let tbl = document.getElementById("topbreakline");
+    if(window.scrollY < 5) {
+        nav.style.transform = "translateY(-75%)";
+        tb.style.transform = "translateY(0%)";
+        tb.style.boxShadow = "0px 5px 1em #000000aa";
+        tbl.style.height = "15em";
+        isUp = false;
+    }
+    else {
+        nav.style.transform = "translateY(-275%)";
+        tb.style.transform = "translateY(-100%)";
+        tbl.style.height = "0";        
+        tb.style.boxShadow = "0";
+        isUp = true;
+    }
+}
+
+window.onload = () => {setDeviceType();}
+window.addEventListener("scroll", (e) => {toggleTab();});
